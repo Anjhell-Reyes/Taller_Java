9bc4edd83012f8a6c6f8180caaaa11f76eab1d66
Anjhell <anjhell123@gmail.com>
2024-05-07 08:18:10 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Anjhell Reyes

--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -1,4 +1,25 @@
 public class FiguraGeometrica {
     private String nombre;
     private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }

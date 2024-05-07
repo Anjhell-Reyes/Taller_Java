2ca0eb792a48f523e2d7a7dc6e16895d4e532379
Anjhell <anjhell123@gmail.com>
2024-05-07 08:10:49 -0500

    Polimorfismo - Implementación en Clase Empleado por Anjhell Reye

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,2 +1,8 @@
-public class Empleado {
+public class Empleado extends Persona implements Trabajador{
+    public Empleado(String nombre, Integer edad, String fechaNacimiento) {
+        super(nombre, edad, fechaNacimiento);
+    }
+    public void realizarTarea(){
+        System.out.println("Realizando tarea laboral");
+    }
 }

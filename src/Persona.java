ea302c9b785096f9a1a2a1a05b99f173cae90b58
Anjhell <anjhell123@gmail.com>
2024-05-07 07:27:29 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por Anjhell Reyes

--- src/Persona.java
+++ src/Persona.java
@@ -2,4 +2,34 @@ public class Persona {
     private String nombre;
     private Integer edad;
     private String fechaNacimiento;
+
+    public Persona(String nombre, Integer edad, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getFechaNacimiento() {
+        return this.fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }

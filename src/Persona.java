552bfe0fb2f653bfd7034cdb927f093c3b6e7447
Anjhell <anjhell123@gmail.com>
2024-05-07 07:52:09 -0500

    Herencia - Sobrescritura del método toString() porAnjhell Reyes

--- src/Persona.java
+++ src/Persona.java
@@ -43,4 +43,8 @@ public class Persona {
         System.out.println("Su anio de nacimiento fue en el: " + (2024 - this.edad));
     }
 
+    public void mensaje(){
+        System.out.println("Mi nombre es: " + this.nombre + " , Mi edad es: " + this.edad);
+    }
+
 }
